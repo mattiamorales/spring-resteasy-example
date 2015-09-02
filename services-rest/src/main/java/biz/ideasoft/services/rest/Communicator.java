eed7c8031872e3e36193184bce97171c566e89c6
Daniel Guerrero <dguerrero@ideasoft.biz>
2015-09-01 23:12:13 -0300

    Cambios simples

--- services-rest/src/main/java/biz/ideasoft/services/rest/Communicator.java
+++ services-rest/src/main/java/biz/ideasoft/services/rest/Communicator.java
@@ -3,6 +3,7 @@ package biz.ideasoft.services.rest;
 import biz.ideasoft.domain.Message;
 import biz.ideasoft.services.core.SimpleMessenger;
 import biz.ideasoft.services.api.Messenger;
+import org.springframework.stereotype.Service;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -10,11 +11,13 @@ import javax.ws.rs.core.Response;
 /**
  * Created by daniel on 31/8/15.
  */
-@Path("/communicator") public class Communicator implements Communication {
+@Path("/communicator")
+@Service public class Communicator implements Communication {
 
   public Response saySomething(String aType) {
     Messenger messenger = new SimpleMessenger();
     Message message = messenger.saySomething(aType);
+//    return Response.ok("{type: \"" + aType + "\"}").build();
     return Response.status(Response.Status.OK).entity(message).build();
   }
 }
